82c8e76102b79389b7f41a6707db4c7013058b05
jayoung977 <wjsrnr20@gmail.com>
2023-04-20 17:29:34 +0900

    study: 리액트 합성이벤트

---
+++ 03_react_eventHandling/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import SyntheticEvent from "./SyntheticEvent";
+
+function App() {
+  return (
+    <div className="App">
+      <SyntheticEvent />
+    </div>
+  );
+}
+export default App;
---
+++ 03_react_eventHandling/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      {/* 컴포넌트 하나(ex.App)밖에 못넣음 */}
+      <App />
+    </React.StrictMode>
+  );
+});
+// }, 1000);
+// 1000: 1초
